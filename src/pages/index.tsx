ac3a4bdafa8d1579b36e43ac186f50dbf2498a20
Felipe <feferreira.sf@gmail.com>
2023-04-19 20:02:28 -0300

    feat: Hero section ready

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { Items } from "../utils/constants";
 import {
   Content,
   HeroContainer,
@@ -7,34 +8,6 @@ import {
   ItemsGroup,
 } from "../styles/pages/home";
 import heroIMG from "../assets/hero_image.png";
-import { Coffee, Package, ShoppingCart, Timer } from "@phosphor-icons/react";
-
-const Items = [
-  {
-    id: 1,
-    variant: "cart",
-    icon: <ShoppingCart weight='fill' />,
-    text: "Compra simples e segura",
-  },
-  {
-    id: 2,
-    variant: "package",
-    icon: <Package weight='fill' />,
-    text: "Embalagem mantém o café intacto",
-  },
-  {
-    id: 3,
-    variant: "delivery",
-    icon: <Timer weight='fill' />,
-    text: "Entrega rápida e rastreada",
-  },
-  {
-    id: 4,
-    variant: "coffee",
-    icon: <Coffee weight='fill' />,
-    text: "O café chega fresquinho até você",
-  },
-];
 
 export default function Home() {
   return (
@@ -50,13 +23,10 @@ export default function Home() {
             <ItemsGroup>
               {Items.map((item) => (
                 <Item key={item.id} shape={item.variant}>
-                  <span>
-                    {item.icon}
-                  </span>
+                  <span>{item.icon}</span>
                   <p>{item.text}</p>
                 </Item>
               ))}
-              {/*  */}
             </ItemsGroup>
           </Content>
           <Image src={heroIMG} alt='' />
