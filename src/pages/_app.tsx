6069bf9bccfe9f5d759c2e2e3c57d6ad6467cf33
Felipe <feferreira.sf@gmail.com>
2023-04-19 14:59:29 -0300

    feat: added navbar import

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,13 @@
 import type { AppProps } from "next/app";
 import { globalStyles } from "../styles/global";
+import Navbar from "@/components/Navbar";
 
 globalStyles();
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+    </>
+  );
 }
