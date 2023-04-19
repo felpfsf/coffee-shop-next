1ae49fe05e3c827e8412eafa9879e90c24f5e20a
Felipe <feferreira.sf@gmail.com>
2023-04-19 12:59:29 -0300

    feat: importing global style

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
-import '@/styles/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { globalStyles } from "../styles/global";
 
+globalStyles();
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
