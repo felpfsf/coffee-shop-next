ee8de8c63b3c0c0525f0beec225eb9f98d357831
Felipe <feferreira.sf@gmail.com>
2023-04-20 16:45:41 -0300

    feat: cart modal with basic product info

--- src/components/CartModal/index.tsx
+++ src/components/CartModal/index.tsx
@@ -1,16 +1,38 @@
 import * as Dialog from "@radix-ui/react-dialog";
 import { CloseButton, Content, Overlay } from "./style";
 import { XCircle } from "@phosphor-icons/react";
+import { useCart } from "../../context/CartContext";
 
 const CartModal = () => {
+  const { cartItems } = useCart();
+  /**
+   * TODO:
+   *
+   * [x] - SOMAR O VALOR TOTAL DE PRODUTOS
+   *
+   * [] - EXIBIR O CARD COM BOTÕES DE ADICIONAR MAIS ITENS
+   *      E REMOVER DO CARRINHO
+   */
+  const subtotal = cartItems.reduce((sum, item) => {
+    return sum + item.quantity * Number(item.product.price);
+  }, 0);
+  console.log(cartItems.map(item => item.product.price))
+  console.log(subtotal)
   return (
     <Dialog.Portal>
       <Overlay />
       <Content>
         <h1>CartModal</h1>
+        <div>
+          {cartItems.map((item) => (
+            <p key={item.product.id}>
+              {item.product.name} unidades:<span>{item.quantity}</span>
+            </p>
+          ))}
+        </div>
         <Dialog.Close asChild>
           <CloseButton>
-            <XCircle size={24} weight="fill" />
+            <XCircle size={24} weight='fill' />
           </CloseButton>
         </Dialog.Close>
       </Content>
